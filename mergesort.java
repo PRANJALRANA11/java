353f3dafbf0b3871e6c9584fb39be2e70d919aae
Pranjalrana <129268721+PRANJALRANA11@users.noreply.github.com>
2023-05-15 17:00:21 +0530

    mergesort

--- mergesort.java
+++ mergesort.java
@@ -1,41 +1,47 @@
-public class mergesort {
-    static void merge(int []arr,int idx,int mid,int r ){
-        int n1=mid-idx+1;
-        int n2=r-mid;
-        int[]left=new int[n1];
-        int []right=new int[n2];
-        int i ,j ,k;
-        for(i=0;i<n1;i++)left[i]=arr[idx+i];
-        for(j=0;j<n2;j++)right[j]=arr[mid+1+j];
-        i=0;
-        j=0;
-        k=0;
-        while(i<n1 && j<n2){
-            if(left[i]<right[j])arr[k++]=left[i++];
-            else{arr[k++]=right[j++];}
+class HelloWorld {
+    static int partition(int []arr,int st,int end){
+        int pivot=arr[st];
+        int cnt=0;
+        int l=st+1;
+        for(int i=st+1;i<=end;i++){
+            if(arr[i]<pivot)cnt++;
         }
-        while(i<n1)arr[k++]=left[i++];
-        while(j<n2)arr[k++]=right[j++];
-    }
-    static void mergesort(int arr[],int idx,int r){
-        if(idx>=r)return;
-        int mid=(idx+r)/2;
-        mergesort(arr,idx,mid);
-        mergesort(arr,mid+1,r);
-        merge(arr,idx,mid,r);
+            int pivotidx=st+cnt;
+            while(l<=end){
+            int temp=pivotidx;
+            pivotidx=arr[l];
+            arr[l]=temp;
+            l++;
+        }
+            int j=st;
+            int k=end;
+            while(j<pivotidx && k>pivotidx){
+                while(arr[j]>pivot||arr[k]<pivot){ 
+                int  temp=arr[j];
+                arr[j]=arr[k];
+                arr[k]=temp;
+                j++;
+                k--;
+                }
+            }return pivot;
         
-    }static void display(int []arr){
-        for(int k:arr)System.out.println(arr[k]);
-    }
-    public static void main(String[] args){
-         int [] arr={2,0,1,0,4};
-        int idx=0;                                         
-        int r=arr.length-1;
-        mergesort(arr,idx,r);
-        display(arr); 
+        }
+    
+    static void quicksort(int[] arr,int st,int end){
+        if(st>=end)return;
+        int pi=partition( arr,st,end);
+        end=pi-1;
+        quicksort(arr,st,end);
+        st=pi+1;
+        quicksort(arr,st,end);
+        for(int i=0;i<=arr.length-1;i++ ){
+            System.out.println(arr);
+        }
+    } 
+    public static void main(String[] args) {
+        int [] arr={2,4,21,123,3,23,432,43,4};
+        int st=0;
+        int end=arr.length-1;
+        quicksort(arr,st,end);
     }
 }
-
-
-    
-
